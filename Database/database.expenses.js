12b1da67cdef5ea640f8c15f589e8e116d7bedc1
Viraj Gavade <vrajgavade17@gmail.com>
2024-11-06 13:10:01 +0530

    "Added DB port to database connections and updated test script in package.json"

--- Database/database.expenses.js
+++ Database/database.expenses.js
@@ -4,7 +4,8 @@ const pool = mysql.createPool({
     host:process.env.DATABASE_HOST,
     user:process.env.DATABASE_USER,
     password:process.env.DATABASE_PASSWORD,
-    database:process.env.DB_NAME
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT
 }).promise()
 
 
--- Database/database.users.js
+++ Database/database.users.js
@@ -10,7 +10,9 @@ const pool = mysql.createPool({
     host:process.env.DATABASE_HOST,
     user:process.env.DATABASE_USER,
     password:process.env.DATABASE_PASSWORD,
-    database:process.env.DB_NAME
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT
+
 }).promise()
 
 
